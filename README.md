e651b6d86b393813bb80bbdc91a77a78d2934874
Kinrany <kinrany@yandex.com>
2019-02-18 00:27:20 +0300

    Draw a red rectangle

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-hammerpipe
+# Hammerpipe
+
+## The idea
+
+[Pipe Mania][pipe-mania], but hitting the pipes with a hammer
+randomly turns and moves other pipes.
+
+[pipe-mania]: https://en.wikipedia.org/wiki/Pipe_Mania
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use quicksilver::{
+  geom::{Rectangle, Vector},
+  graphics::{Background, Color},
+  lifecycle::{run, Settings, State, Window},
+  Result,
+};
+
+struct Game;
+
+impl State for Game {
+  /// Load the assets and initialise the game
+  fn new() -> Result<Self> {
+    Ok(Self)
+  }
+
+  /// Process keyboard and mouse, update the game state
+  fn update(&mut self, _window: &mut Window) -> Result<()> {
+    Ok(())
+  }
+
+  /// Draw stuff on the screen
+  fn draw(&mut self, window: &mut Window) -> Result<()> {
+
+    let area = Rectangle::new((50, 50), (100, 200));
+    let background = Background::Col(Color::RED);
+    window.draw(&area, background);
+
+    Ok(())
+  }
+}
+
 fn main() {
-    println!("Hello, world!");
+  std::env::set_var("WINIT_HIDPI_FACTOR", "1.0");
+  let settings = Settings {
+    scale: quicksilver::graphics::ImageScaleStrategy::Blur,
+    ..Default::default()
+  };
+  run::<Game>("Quicksilver Roguelike", Vector::new(800, 600), settings);
 }

052ebfca2437a85f7cbe6091ac11e3604877de11
Kinrany <kinrany@yandex.com>
2019-02-20 01:17:58 +0300

    Refactor

--- src/game.rs
+++ src/game.rs
@@ -5,44 +5,67 @@ use quicksilver::{
   Result,
 };
 
-mod field {
-  use super::*;
+struct Field;
 
-  pub fn size(window: &Window) -> Vector {
-    let screen_size = window.screen_size();
+impl Field {
+  pub fn size(w: &Window) -> Vector {
+    let screen_size = w.screen_size();
     Vector::ONE * f32::min(screen_size.x, screen_size.y)
   }
 
-  pub fn offset(window: &Window) -> Vector {
-    (window.screen_size() - size(window)) / 2
+  pub fn pos(w: &Window) -> Vector {
+    (w.screen_size() - Field::size(w)) / 2
   }
 
-  pub fn rect(window: &Window) -> Rectangle {
-    Rectangle::new(offset(window), size(window))
+  pub fn rect(w: &Window) -> Rectangle {
+    Rectangle::new(Field::pos(w), Field::size(w))
+  }
+}
+
+pub const CELL_COUNT: u32 = 6;
+pub const SPACE_BETWEEN_CELLS: f32 = 0.3;
+pub const GRID_WIDTH_IN_CELLS: f32 = CELL_COUNT as f32 + SPACE_BETWEEN_CELLS * (CELL_COUNT - 1) as f32;
+
+pub struct Cell {
+  pub x: u32,
+  pub y: u32,
+}
+
+impl Cell {
+  pub fn size(w: &Window) -> Vector {
+    Field::size(w) / GRID_WIDTH_IN_CELLS
   }
 
-  pub fn draw_background(window: &mut Window) {
-    let color = Color {r: 0.5, g: 0.8, b: 0.5, a: 1.0};
-    let background = Background::Col(color);
-    window.draw(&rect(window), background);
+  pub fn pos(&self, w: &Window) -> Vector {
+    let cell_size_with_space = Cell::size(w) * (1.0 + SPACE_BETWEEN_CELLS);
+    let offset = Vector::new(
+      self.x as f32 * cell_size_with_space.x,
+      self.y as f32 * cell_size_with_space.y,
+    );
+    Field::pos(w) + offset
   }
 
-  pub fn draw_grid(window: &mut Window, cell_count: u32, space: f32) {
-    let cell_color = Color {r: 0.25, g: 0.4, b: 0.5, a: 1.0};
-    let background = Background::Col(cell_color);
-
-    let grid_width_in_rows: f32 = cell_count as f32 + space * (cell_count - 1) as f32;
-    let cell_width = size(window).x / grid_width_in_rows;
-    let offset_size = cell_width * (1.0 + space);
-
-    for x in 0..cell_count {
-      for y in 0..cell_count {
-        let pos = offset(window) + Vector::new(x, y) * offset_size;
-        let rect = Rectangle::new(pos, Vector::ONE * cell_width);
-        window.draw(&rect, background);
-      }
-    };
+  pub fn rect(&self, w: &Window) -> Rectangle {
+    Rectangle::new(self.pos(w), Cell::size(w))
   }
+}
+
+pub fn draw_background(w: &mut Window) {
+  let color = Color {r: 0.5, g: 0.8, b: 0.5, a: 1.0};
+  let background = Background::Col(color);
+  w.draw(&Field::rect(w), background);
+}
+
+pub fn draw_cells(w: &mut Window) {
+  let cell_color = Color {r: 0.25, g: 0.4, b: 0.5, a: 1.0};
+  let background = Background::Col(cell_color);
+
+  for x in 0..CELL_COUNT {
+    for y in 0..CELL_COUNT {
+      let cell = Cell {x, y};
+      w.draw(&cell.rect(w), background);
+    }
+  };
 }
 
 pub struct Game;
@@ -66,8 +89,8 @@ impl State for Game {
       Background::Col(Color::BLACK),
     );
 
-    field::draw_background(window);
-    field::draw_grid(window, 6, 0.3);
+    draw_background(window);
+    draw_cells(window);
 
     Ok(())
   }
